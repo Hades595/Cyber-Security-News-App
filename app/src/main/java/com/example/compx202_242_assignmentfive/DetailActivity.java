5feb4cda93b9c4ab655587225f91ddc202907cf1
Chirag Mehta <cm298@students.waikato.ac.nz>
2021-09-14 22:46:55 +1200

    Implemented DetailActivity dynamically

--- app/src/main/java/com/example/compx202_242_assignmentfive/DetailActivity.java
+++ app/src/main/java/com/example/compx202_242_assignmentfive/DetailActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
@@ -14,8 +16,24 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         Intent intent = getIntent();
-        String position = intent.getStringExtra("position");
-        Toast.makeText(this, position, Toast.LENGTH_SHORT).show();
+        int position = intent.getIntExtra("position", 0);
+        //Toast.makeText(this, position, Toast.LENGTH_SHORT).show();
+
+        ImageView bannerView = (ImageView)findViewById(R.id.detail_activity_imageview_banner);
+        TextView titleView = (TextView) findViewById(R.id.detail_activity_textview_title);
+        TextView contentView = (TextView) findViewById(R.id.detail_activity_textview_content);
+
+        String title = getResources().getStringArray(R.array.string_array_titles)[position];
+        String content = getResources().getStringArray(R.array.string_array_content)[position];
+        String banner = getResources().getStringArray(R.array.string_array_banners)[position];
+
+        int res = getResources().getIdentifier(banner, "drawable", this.getPackageName());
+        bannerView.setImageResource(res);
+
+        titleView.setText(title);
+        contentView.setText(content);
+
+
 
     }
 }
