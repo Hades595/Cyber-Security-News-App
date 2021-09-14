dd011a038ad5ade220c87c732cb364c46b348074
Chirag Mehta <cm298@students.waikato.ac.nz>
2021-09-14 18:09:31 +1200

    Created List Activity

---
+++ app/src/main/java/com/example/compx202_242_assignmentfive/ListActivity.java
@@ -0,0 +1,14 @@
+package com.example.compx202_242_assignmentfive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
