290796fe786a786778a8b90dd40fc8eed1d3f1fd
Chirag Mehta <cm298@students.waikato.ac.nz>
2021-09-14 21:09:20 +1200

    Added ArrayAdapter

--- app/src/main/java/com/example/compx202_242_assignmentfive/ListActivity.java
+++ app/src/main/java/com/example/compx202_242_assignmentfive/ListActivity.java
@@ -3,6 +3,8 @@ package com.example.compx202_242_assignmentfive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        String[] array = getResources().getStringArray(R.array.string_array_titles);
+        ArrayAdapter<String> listItems = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, array);
+
+        ListView listView = (ListView)findViewById(R.id.listview_scms);
+        listView.setAdapter(listItems);
+
+
     }
+
+
 }
